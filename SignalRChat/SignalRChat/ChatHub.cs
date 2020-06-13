ab9540b0b3e335b4239019240b604824d5134d0e
Eric Wong <eric.t.wong@avanade.com>
2020-06-13 17:18:58 -0400

    feat: signalR poc

---
+++ SignalRChat/SignalRChat/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        public void SendToOthers(string name, string message)
+        {
+            // Call the broadcastMessage method to update other clients.
+            Clients.Others.broadcastMessage(name, message);
+        }
+
+        public void SendDiagnostics()
+        {
+            // Call the broadcastMessage method to update other clients.
+            Clients.All.diagnostics(Context.ConnectionId);
+        }
+    }
+}
